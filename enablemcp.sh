84a9d2fbdb7419fd6f3f3874da326ba9634b8149
xbill <xbill9@gmail.com>
2025-09-10 16:59:55 -0400

    add support scripts

--- enablemcp.sh
+++ enablemcp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source ~/gemini-cli-codeassist/set_env.sh
+
+echo "Fetching Standard Cymbal SuperStore"
+gsutil -m cp -rp gs://duet-appdev/cymbal-superstore .
+
 echo "Enabling Pre-Built MCP Support "
 
 cp index.ts.mcp cymbal-superstore/backend/index.ts
 cp package.json.mcp cymbal-superstore/backend/package.json
-
-cd cymbal-superstore/backend
-npm init
 
 
 
