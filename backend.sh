3b21ebfb8d0f7da07090195b9c93c1b49b852e9d
xbill9 <xbill@glitnir.com>
2025-09-10 20:19:38 -0400

    update to force unauthenticated

--- backend.sh
+++ backend.sh
@@ -8,8 +8,7 @@ echo "Building Docker Image"
 docker push gcr.io/$PROJECT_ID/cymbal-superstore-inventory-api:latest
 
 echo "Deploying to Cloud Run"
-gcloud run deploy inventory --image=gcr.io/$PROJECT_ID/cymbal-superstore-inventory-api --port=8000 --region=us-central1 --allow
--unauthenticated
+gcloud run deploy inventory --image=gcr.io/$PROJECT_ID/cymbal-superstore-inventory-api --port=8000 --region=us-central1 --allow-unauthenticated
 
 
 
