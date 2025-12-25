3c43ed40657d3e30c301a070bf99a983ed58b146
xbill9 <xbill@glitnir.com>
2025-12-25 14:49:45 -0500

    first c# build

---
+++ mcp-stdio-csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+[McpServerToolType]
+public static class MyTools
+{
+    [McpServerTool]
+    public static string Greet(string name)
+    {
+        return $"Hello, {name}!";
+    }
+}
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Logging.AddConsole(consoleLogOptions =>
+        {
+            // Configure all logs to go to stderr
+            consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+        });
+
+        builder.Services.AddMcpServer()
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly();
+
+        using IHost host = builder.Build();
+        await host.RunAsync();
+    }
+}
